97333da9c9eb8e5f0096131df97e1030a3b79121
yamishaborisov <borisov3676@gmail.com>
2025-08-07 18:51:41 +0300

    add button to posts.tsx, add canvas

---
+++ src/features/canvas/ui/index.tsx
@@ -0,0 +1,65 @@
+import { useRef, useEffect, useState } from 'react';
+import styles from './style.module.css';
+import MyButton from '@shared-ui/button/button';
+
+type Props = {
+	width?: number;
+	height?: number;
+};
+
+export default function Canvas({ width = 600, height = 400 }: Props) {
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+	const contextRef = useRef<CanvasRenderingContext2D | null>(null);
+	const [isDrawing, setIsDrawing] = useState(false);
+
+	useEffect(() => {
+		const context = contextRef.current;
+		if (!context) return;
+		context.lineWidth = 2;
+		context.lineCap = 'round';
+		context.strokeStyle = 'red';
+	}, []);
+
+	const startDrawing = (e: React.MouseEvent) => {
+		const context = contextRef.current;
+		context?.beginPath();
+		context?.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+		setIsDrawing(true);
+	};
+
+	const draw = (e: React.MouseEvent) => {
+		if (!isDrawing) return;
+
+		const context = contextRef.current;
+
+		context?.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+		context?.stroke();
+	};
+
+	const stopDrawing = () => {
+		setIsDrawing(false);
+	};
+
+	const clearCanvas = () => {
+		const context = contextRef.current;
+		context?.clearRect(0, 0, width, height);
+	};
+
+	return (
+		<>
+			<canvas
+				className={styles.canvas}
+				ref={canvasRef}
+				width={width}
+				height={height}
+				onMouseDown={startDrawing}
+				onMouseMove={draw}
+				onMouseUp={stopDrawing}
+				onMouseLeave={stopDrawing}
+			>
+				Ваш браузер не поддерживает canvas
+			</canvas>
+			<MyButton onClick={clearCanvas}>Очистить холст</MyButton>
+		</>
+	);
+}
