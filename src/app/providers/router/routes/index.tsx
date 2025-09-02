1c981555e960fe3c1ec2524794bb928052efffa4
yamishaborisov <borisov3676@gmail.com>
2025-09-02 20:45:18 +0300

    feat(5) add uncontrolled page, using useOptimistic, useId

--- src/app/providers/router/routes/index.tsx
+++ src/app/providers/router/routes/index.tsx
@@ -4,6 +4,7 @@ import { PostIdPage } from '@/pages/post-id-page';
 import { Posts } from '@/pages/posts';
 import { CanvasPage } from '@/pages/canvas';
 import { ControlledForm } from '@/pages/controlled-form';
+import { UncontrolledForm } from '@/pages/uncontrolled-form';
 
 export const routes = [
 	{ path: '/login', element: <Login />, private: false },
@@ -12,4 +13,5 @@ export const routes = [
 	{ path: '/posts/:id', element: <PostIdPage />, private: true },
 	{ path: '/canvas', element: <CanvasPage />, private: true },
 	{ path: '/controlled-form', element: <ControlledForm />, private: true },
+	{ path: '/uncontrolled-form', element: <UncontrolledForm />, private: true },
 ];
